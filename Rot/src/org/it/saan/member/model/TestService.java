638a24ef2536d47d4edc543de65396ea1376f669
Hoon <lasmonatte@gmail.com>
2015-08-04 19:56:01 +0900

    dadsf

--- Rot/src/org/it/saan/member/model/TestService.java
+++ Rot/src/org/it/saan/member/model/TestService.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TestService {
-
-		@Autowired
+		
 		TestDao testDao;
-
 		public int service() {
 			System.out.println("SAfasfsafsafa");
 			return testDao.readAll();
